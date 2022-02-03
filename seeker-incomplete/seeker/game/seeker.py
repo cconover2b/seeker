581b98f4ac1afb32b6c4ebd641b0459a839c0df6
cconover2b <97270528+cconover2b@users.noreply.github.com>
2022-02-02 21:16:35 -0700

    Completed the file. It works

--- seeker-incomplete/seeker/game/seeker.py
+++ seeker-incomplete/seeker/game/seeker.py
@@ -1,4 +1,4 @@
-
+import random
 
 class Seeker:
     """The person looking for the Hider. 
@@ -11,25 +11,26 @@ class Seeker:
 
 
     def __init__(self):
-            """Constructs a new Seeker.
+        """Constructs a new Seeker.
 
-            Args:
-                self (Seeker): An instance of Seeker.
-            """
-            pass
+        Args:
+            self (Seeker): An instance of Seeker.
+        """
+        self._location = random.randint(1, 1000)
         
     def get_location(self):
-            """Gets the current location.
-            
-            Returns:
-                number: The current location,
-            """
+        """Gets the current location.
+        
+        Returns:
+            number: The current location,
+        """
+        return self._location
             
     def move_location(self, location):
+        """Moves to the given location.
 
-            """Moves to the given location.
-
-            Args:
-                self (Seeker): An instance of Seeker.
-                location (int): The given location.
-            """
+        Args:
+            self (Seeker): An instance of Seeker.
+            location (int): The given location.
+        """
+        self._location = location
